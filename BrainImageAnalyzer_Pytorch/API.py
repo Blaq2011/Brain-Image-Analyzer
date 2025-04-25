3fe234fb5298793d4cf46a7c6036663be4d6f46d
Evans Siaw <blaqfps@gmail.com>
2025-04-25 09:34:08 +0200

    removed port config

--- BrainImageAnalyzer_Pytorch/API.py
+++ BrainImageAnalyzer_Pytorch/API.py
@@ -47,9 +47,9 @@ async def predict_image(file: UploadFile = File(...)):
 
 
 
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run("BrainImageAnalyzer_Pytorch.API:app", host="0.0.0.0", port=10000, reload=False)
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run("BrainImageAnalyzer_Pytorch.API:app", host="0.0.0.0", port=10000, reload=False)
 
 # if __name__ == "__main__":
 #     uvicorn.run("API:app", host="127.0.0.1", port=8080, reload=True)
